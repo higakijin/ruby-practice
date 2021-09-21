f32d58d3f0350b7e6eff5164a615f3117ab488c6
higakijin <hm385.chejptks@gmail.com>
2021-09-22 00:08:52 +0900

    make fizzbuzz with using each method

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
-i = 1
-while i <= ARGV[0].to_i
-  if i%15 == 0
+num = ARGV[0].to_i
+
+(1..num).each do |i|
+  if i % 15 == 0
     puts "FizzBuzz"
-  elsif i%5 == 0
+  elsif i % 5 == 0
     puts "Buzz"
-  elsif i%3 == 0
+  elsif i % 3 == 0
     puts "Fizz"
   else
     puts i
   end
-  i += 1
 end
---
+++ sort.rb
@@ -0,0 +1,5 @@
+lists = [10, 8, 3, 5, 2, 4, 11, 18, 20, 33]
+
+lists.each do |num|
+  num < 
+end
